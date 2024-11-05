22635db6a7e4ed1c4e5c48c99617ddb0e1621392
akhil.kurup <akhil.kurup@inseinc.com>
2024-11-05 17:51:39 +0530

    minor changes

---
+++ bookingconfirmationApp/src/main/java/org/airkerala/BookingScanner.java
@@ -0,0 +1,17 @@
+package org.airkerala;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class BookingScanner {
+
+	
+	@PostConstruct
+	public void startScanning()
+	{
+		
+	}
+	
+}
---
+++ bookingconfirmationApp/src/main/java/org/airkerala/controller/BookingConfirmController.java
@@ -0,0 +1,5 @@
+package org.airkerala.controller;
+
+public class BookingConfirmController {
+
+}
